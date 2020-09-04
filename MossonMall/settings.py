9a026f952a5e35b135e8172d670990f1dfe003b2
mossonli <mc136782643@gmail.com>
2020-09-04 18:28:05 +0800

    表结构

--- MossonMall/settings.py
+++ MossonMall/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
